7796616fa6c5c3ce8e5d6b196f24bc19e504dd74
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-06-26 21:42:41 -0400

    Add methods to add and delete recipes with current logged user and to list it.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,39 @@
 class RecipesController < ApplicationController
+    before_action :authenticate_user!
+  
     def index
+      @recipes = current_user.recipes
     end
+  
     def show
+      @recipe = Recipe.find(params[:id])
+      @foods = @recipe.foods
     end
-end
+  
+    def new
+      @recipe = Recipe.new
+    end
+  
+    def create
+      @recipe = current_user.recipes.build(recipe_params)
+  
+      if @recipe.save
+        redirect_to @recipe, notice: 'Recipe was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      @recipe = Recipe.find(params[:id])
+      @recipe.destroy if @recipe.user == current_user
+      redirect_to recipes_path
+    end
+  
+    private
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
+  end
+  
