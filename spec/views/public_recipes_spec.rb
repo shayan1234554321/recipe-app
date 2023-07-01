4d6bc05361d3bd476019b405269da912118c74ac
shayan1234554321 <kshayan091@gmail.com>
2023-07-01 16:05:59 +0500

    Linter fix

--- spec/views/public_recipes_spec.rb
+++ spec/views/public_recipes_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Public Recipe Index Page', type: :feature do
-
-    before do
-        visit public_recipes_path
-    end
+  before do
+    visit public_recipes_path
+  end
 
   it 'Displays content' do
     expect(page).to have_content('Public Recipes')
