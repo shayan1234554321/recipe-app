26d46c498ebdba59bd07eb8e78941efd600fcf0e
shayan1234554321 <kshayan091@gmail.com>
2023-07-01 17:32:23 +0500

    Added tests for controllers

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module RecipeApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.autoload_paths += %W(#{config.root}/app/models)
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
